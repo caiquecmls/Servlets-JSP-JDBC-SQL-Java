2831e90773b6fa0378d591f2d8cfad9e5f0afa0f
Caique Moreira <caique_moreira7@hotmail.com>
2021-07-15 04:52:22 -0300

    criando o model de login e fazendo simulacao de login no sistema

---
+++ src/servlets/ServletLogin.java
@@ -0,0 +1,58 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.ModelLogin;
+
+/*O chamado controler são as servlets ou ServletLoginController*/
+@WebServlet("/ServletLogin") // Mapeamento de URL que vem da tela
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ServletLogin() {
+		super();
+
+	}
+
+	/* Recebe os dados pela url em parametros */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	/* Recebe os dados enviados por formulario */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		if(!login.isEmpty() && login != null && !senha.isEmpty() && senha != null) {
+			ModelLogin modelLogin =new  ModelLogin();			
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+			
+			if(modelLogin.getLogin().equalsIgnoreCase("admin") && modelLogin.getSenha().equalsIgnoreCase("admin")) {
+				request.getSession().setAttribute("usuario", modelLogin.getLogin()); //MANTER USUÁRIO NA SESSÃO
+				RequestDispatcher redirecionarPrincipal = request.getRequestDispatcher("principal/principal.jsp");
+				redirecionarPrincipal.forward(request, response);
+			}else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Informe o login e senha corretamente!");
+				redirecionar.forward(request, response);
+			}
+		}else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe o login e senha corretamente!");
+			redirecionar.forward(request, response);
+		}
+		
+
+	}
+
+}
